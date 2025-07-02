357883e33488434439e64b6605c42aa019d926c6
Obiora-Okeke <obioraokeke03@gmail.com>
2025-07-01 20:25:51 -0700

    Adding team member names

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
+  <div style={{ padding: '2em' }}>
     <h1>Welcome to Throwing Errors!</h1>
-    <p>Explore, rate, and enjoy creative recipes.</p>
+    <p>Welcome to our site! This is supposed to be a "cookbook" type site, displaying recipes.</p>
+    <p>Users will be able to search for recipes, as well as leave ratings.</p>
+    <p>We may hold off on including full recipes until the project is more fleshed out, but they are available in the database!</p>
+
+    <h2>Meet the Team</h2>
+    <ul>
+      <li>Obiora Okeke</li>
+      <li>Benjamin Pable</li>
+    </ul>
   </div>
 );
 
